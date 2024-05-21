01889a70e4b38b112514a938df7119910b67cdcd
christina.lu3799@gmail.com <christina.lu3799@gmail.com>
2024-05-20 20:23:15 -0400

    add post route to register new user

--- server/index.js
+++ server/index.js
@@ -2,11 +2,32 @@ import express from 'express';
 import { PORT, mongoDBURL } from './config.js';
 import mongoose from 'mongoose';
 
+import { User } from './models/User.js';
+
 const app = express();
 
+app.use(express.json());
 app.get('/', (req, res) => {
     console.log(req);
-    return res.status(234).send('Welcome');
+    return res.status(200).send('Welcome');
+});
+
+app.post('/register', async (req, res) => {
+    const { name, email, password } = req.body;
+    try {
+        if (!name || !email || !password) {
+            return response.status(400).send({
+                message: 'Missing required fields.',
+            });
+        }
+
+        const newUser = await User.create({ name, email, password });
+
+        return res.status(201).send(newUser);
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
 });
 
 mongoose

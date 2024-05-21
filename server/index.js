703f675c18c17ff7cdadda1f49fbbd3693eca51f
christina.lu3799@gmail.com <christina.lu3799@gmail.com>
2024-05-20 20:05:51 -0400

    connect to mongoose db

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import { PORT } from './config.js';
+import { PORT, mongoDBURL } from './config.js';
+import mongoose from 'mongoose';
 
 const app = express();
 
@@ -8,6 +9,14 @@ app.get('/', (req, res) => {
     return res.status(234).send('Welcome');
 });
 
-app.listen(PORT, () => {
-    console.log(`Listening to port: ${PORT} 🎧 `);
-});
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log('Successfully connected to database.');
+        app.listen(PORT, () => {
+            console.log(`Listening to port: ${PORT} 🎧 `);
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });

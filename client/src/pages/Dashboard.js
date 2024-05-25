2774e9be7c054b9460fd000b2e1cb08580e2248f
christina.lu3799@gmail.com <christina.lu3799@gmail.com>
2024-05-25 11:10:21 -0400

    add logout btn to dashboard page

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,12 +1,26 @@
 import { Navigate } from 'react-router-dom';
 import { useAuthentication } from '../hooks/useAuthentication';
+import Nav from 'react-bootstrap/Nav';
 
-const Dashboard = ({ children }) => {
+const Dashboard = () => {
+    const { logout } = useAuthentication();
+    const handleLogout = () => {
+        logout();
+    };
     const { user } = useAuthentication();
     if (!user) {
         return <Navigate to='/login' />;
     }
-    return children;
+    return (
+        <div className='bg-pink-100 h-[100vh] flex flex-col justify-center items-center'>
+            <p className='text-8xl mb-12 font-gloria'>Dashboard</p>
+            <Nav.Item>
+                <Nav.Link onClick={handleLogout} className='text-dark'>
+                    LOGOUT
+                </Nav.Link>
+            </Nav.Item>
+        </div>
+    );
 };
 
 export default Dashboard;

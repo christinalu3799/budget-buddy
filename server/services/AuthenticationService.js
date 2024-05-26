0122512b4b73ca367e4297c45956aae184fa5fe5
christina.lu3799@gmail.com <christina.lu3799@gmail.com>
2024-05-26 09:06:08 -0400

    move crud operations to AuthenticationService

--- server/services/AuthenticationService.js
+++ server/services/AuthenticationService.js
@@ -5,6 +5,27 @@ import passport from 'passport';
 class AuthenticationService {
     constructor() {}
 
+    async findUsers() {
+        const users = await User.find({});
+        return users;
+    }
+
+    async findUser(id) {
+        const user = await User.findById(id);
+        return user;
+    }
+
+    // separate into update username vs pw
+    async updateUser({ id, body }) {
+        const updatedUser = await User.findByIdAndUpdate(id, body);
+        return updatedUser;
+    }
+
+    async deleteUser(id) {
+        const userToDelete = await User.findByIdAndDelete(id);
+        return userToDelete;
+    }
+
     async registerNewUser({ name, email, password }) {
         const hashedPassword = await bcrypt.hash(password, 10);
         const newUser = await User.create({
@@ -26,11 +47,23 @@ class AuthenticationService {
                     return next(err);
                 }
                 return res.status(200).send({
-                    authenticated: true,
+                    authenticated: req.isAuthenticated(),
                     message: 'Login successfully',
                 });
             });
         })(req, res, next);
+    }
+
+    async logoutUser(req, res) {
+        await req.logout((err) => {
+            if (err) {
+                return next(err);
+            }
+            return res.status(200).send({
+                authenticated: req.isAuthenticated(),
+                message: 'Logout successfully',
+            });
+        });
     }
 }
 

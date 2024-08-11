0b086f3a9102e79f1dd4e7c0d47fa05cd1362f9f
christina.lu3799@gmail.com <christina.lu3799@gmail.com>
2024-08-10 23:24:21 -0400

    update home page color

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,7 +3,7 @@ import '../App.css';
 
 const Home = () => {
     return (
-        <div className='bg-pink-100 h-[100vh] flex flex-col justify-center items-center'>
+        <div className='h-[100vh] flex flex-col justify-center items-center'>
             <p className='text-8xl mb-12 font-gloria'>Home</p>
             <p>Please login or register to continue.</p>
         </div>

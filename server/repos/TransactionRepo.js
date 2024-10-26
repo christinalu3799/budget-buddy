c1ee9c082819239cbab2758a76aaee40e2cae0d4
christina.lu3799@gmail.com <christina.lu3799@gmail.com>
2024-10-26 09:50:51 -0400

    updates to transaction repo

--- server/repos/TransactionRepo.js
+++ server/repos/TransactionRepo.js
@@ -10,15 +10,15 @@ class TransactionRepo {
             });
         }
 
-        const { userId, accountId, amount } = req.body;
+        const { accountId, amount } = req.body;
         try {
-            if (!userId || !accountId || !amount) {
+            if (!accountId || !amount) {
                 return res.status(400).send({
                     message: 'Missing required fields.',
                 });
             }
 
-            await Transaction.create({
+            const transaction = await Transaction.create({
                 userId: req.user.id,
                 accountId,
                 amount,
@@ -26,38 +26,48 @@ class TransactionRepo {
 
             return res.status(201).send({
                 message: 'Successfully created a new transaction',
-                isAuthenticated: req.isAuthenticated(),
+                transaction,
             });
         } catch (e) {
             return this._throwRequestErrorAndResponse(e, res);
         }
     }
 
-    // async update(req, res) {
-    //     const { userId, account, name, balanceAdjustment } = req.body;
-    //     try {
-    //         if (!userId || !account || !name || !balanceAdjustment) {
-    //             return res.status(400).send({
-    //                 message: 'Missing required fields.',
-    //             });
-    //         }
+    async update(req, res) {
+        if (!req.isAuthenticated()) {
+            return res.status(400).send({
+                message: 'You are not logged in.',
+            });
+        }
 
-    //         const updatedBalance = account.balance + balanceAdjustment;
-    //         const updatedAccount = await Account.findByIdAndUpdate(account.id, {
-    //             balance: updatedBalance,
-    //         });
+        const { transactionId, amount } = req.body;
+        try {
+            if (!transactionId || !amount) {
+                return res.status(400).send({
+                    message: 'Missing required fields.',
+                });
+            }
 
-    //         if (!updatedAccount) {
-    //             return res.status(404).send({ message: 'Account not found.' });
-    //         }
+            const updatedAmount = amount;
+            const updatedTransaction = await Transaction.findByIdAndUpdate(
+                transactionId,
+                { amount: updatedAmount }
+            );
 
-    //         return res
-    //             .status(201)
-    //             .send({ message: 'Successfully updated account.' });
-    //     } catch (e) {
-    //         return this._throwRequestErrorAndResponse(e, res);
-    //     }
-    // }
+            if (!updatedTransaction) {
+                return res
+                    .status(404)
+                    .send({ message: 'Transaction not found.' });
+            }
+
+            return res.status(201).send({
+                message: 'Successfully updated transaction.',
+                updatedTransaction,
+            });
+        } catch (e) {
+            return this._throwRequestErrorAndResponse(e, res);
+        }
+    }
 
     // async delete(req, res) {
     //     if (!req.isAuthenticated()) {
